5d767f28be8d9371a98b90e0ffbfdfb27eebdad5
Shammi Anand <shammianand101@gmail.com>
2025-01-25 16:28:27 +0530

    feat: support simple dir walking and storing to an output file

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"github.com/shammianand/rtt/pkg/walker"
 	"github.com/shammianand/rtt/utils/logger"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
-	rootCmd = &cobra.Command{
+	outputFile string
+	rootCmd    = &cobra.Command{
 		Use:     "rtt",
 		Short:   "Converts a directory to a text file",
 		Long:    `rtt is a CLI tool that converts a directory to a text file with the same name as the directory.`,
@@ -18,11 +21,32 @@ var (
 				return
 			}
 
-			logger.Log.Info("Hello from rtt", "args", args)
+			if len(args) > 1 {
+				logger.Log.Error("only one argument is allowed")
+				return
+			}
+
+			// logger.Log.Info("author: ", viper.GetString("author"))
+			// logger.Log.Info("output: ", outputFile)
+			// logger.Log.Info("args: ", args)
+
+			if err := walker.WalkAndExtract(args[0], outputFile); err != nil {
+				logger.Log.Error(err)
+			}
 		},
 	}
 )
 
 func Exectue() {
 	rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringP("author", "a", "Shammi Anand", "author name for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&outputFile, "output", "o", "rtt-<dir_name>.txt", "output file name")
+
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.SetDefault("author", "Shammi Anand shammianand101@gmail.com")
+	viper.SetDefault("license", "apache")
+
 }
---
+++ pkg/walker/parse.go
@@ -0,0 +1,62 @@
+package walker
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/shammianand/rtt/utils/logger"
+)
+
+// WalkAndExtract walks the given path and extracts the contents to the output file
+func WalkAndExtract(walkPath string, outputPath string) error {
+
+	var buf []byte
+
+	err := filepath.WalkDir(walkPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			logger.Log.Warnf("Error accessing path %q: %v", path, err)
+			return nil
+		}
+
+		if d.Name()[0] == '.' {
+			if d.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		if !d.IsDir() {
+
+			if d.Type() == fs.ModeSymlink {
+				logger.Log.Warnf("Skipping symlink: %s", path)
+				return nil
+			}
+
+			content, err := os.ReadFile(path)
+			if err != nil {
+				logger.Log.Warnf("reading file %q: %v", path, err)
+				return nil
+			}
+			logger.Log.Debugf("File: %s | Size: %d bytes ", path, len(content))
+
+			buf = append(buf, []byte(fmt.Sprintf("[%s]\n```%s\n", path, filepath.Ext(path)[1:]))...)
+			buf = append(buf, content...)
+			buf = append(buf, []byte("\n```\n\n")...)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("Error walking directory: %v", err)
+	}
+
+	if err := os.WriteFile(outputPath, buf, 0644); err != nil {
+		return fmt.Errorf("Error writing to file: %v", err)
+	}
+
+	logger.Log.Infof("successfully written to file: %s | size: %d bytes", outputPath, len(buf))
+
+	return nil
+}
