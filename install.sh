5cf8993fb50b02530e22864577bbf5f087023b2a
Shammi Anand <shammianand101@gmail.com>
2025-01-25 16:55:53 +0530

    fix: fetch correct latest tag

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ BINARY_NAME="rtt"
 
 echo "Installing $BINARY_NAME..."
 
-# Construct download URL
-LATEST_RELEASE_URL="https://github.com/$GITHUB_REPO/releases/latest/download/$BINARY_NAME"
+LATEST_TAG=$(curl -s https://api.github.com/repos/$GITHUB_REPO/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+LATEST_RELEASE_URL="https://github.com/$GITHUB_REPO/releases/download/$LATEST_TAG/$BINARY_NAME"
 
-# Download and install
 sudo curl -fL "$LATEST_RELEASE_URL" -o "/usr/local/bin/$BINARY_NAME"
 sudo chmod +x "/usr/local/bin/$BINARY_NAME"
 
